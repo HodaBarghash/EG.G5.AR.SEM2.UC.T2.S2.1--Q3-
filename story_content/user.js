292432e11b8b919ac241a4c1d46373474f708a12
HodaBarghash <121548764+HodaBarghash@users.noreply.github.com>
2023-02-22 15:06:09 +0200

    g

---
+++ story_content/user.js
@@ -0,0 +1,47 @@
+function ExecuteScript(strId)
+{
+  switch (strId)
+  {
+      case "6ELwNdgdn5E":
+        Script1();
+        break;
+      case "6kB5Lx0QGgA":
+        Script2();
+        break;
+  }
+}
+
+function Script1()
+{
+  const url="https://script.google.com/macros/s/AKfycbx9s1luzQAlPrqtPiHh2vswxgBnmXr4sl4HWEiYO_JXDW-prsjx38ESlOdkHfq_jpWf/exec"
+
+var player=GetPlayer()
+
+fetch(url,{
+	method:"POST",
+	mode: 'no-cors',
+	cache:'no-cache',
+	headers: {'Content-Type': 'application/json'},
+	redirect:'follow',
+	body: JSON.stringify({q1attempts:player.GetVar("q1attempts"),q1answeredcorrect:player.GetVar("q1answeredcorrect")})
+	}
+	)
+}
+
+function Script2()
+{
+  const url="https://script.google.com/macros/s/AKfycbx9s1luzQAlPrqtPiHh2vswxgBnmXr4sl4HWEiYO_JXDW-prsjx38ESlOdkHfq_jpWf/exec"
+
+var player=GetPlayer()
+
+fetch(url,{
+	method:"POST",
+	mode: 'no-cors',
+	cache:'no-cache',
+	headers: {'Content-Type': 'application/json'},
+	redirect:'follow',
+	body: JSON.stringify({q1attempts:player.GetVar("q1attempts"),q1answeredcorrect:player.GetVar("q1answeredcorrect")})
+	}
+	)
+}
+
